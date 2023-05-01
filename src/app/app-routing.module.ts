2e698e8cc342414ecf41308b9141fd4c1801a775
Matheus Chaves <matheusmchaves@spo.ogmaster.local>
2023-05-01 18:51:17 -0300

    crud de pessoas com localStorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
+import { EditarPessoaComponent } from './pessoa/editar-pessoa/editar-pessoa.component';
+import { InserirPessoaComponent } from './pessoa/inserir-pessoa/inserir-pessoa.component';
+import { ListarPessoaComponent } from './pessoa/listar-pessoa/listar-pessoa.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pessoas/listar',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pessoas',
+    redirectTo: 'pessoas/listar',
+  },
+  {
+    path: 'pessoas/listar',
+    component: ListarPessoaComponent,
+  },
+  {
+    path: 'pessoas/novo',
+    component: InserirPessoaComponent,
+  },
+  {
+    path: 'pessoas/editar/:id',
+    component: EditarPessoaComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
