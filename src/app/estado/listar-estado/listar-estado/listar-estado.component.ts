572354df30f701e84ed550a3fac69281e2968699
Matheus Chaves <matheusmchaves@spo.ogmaster.local>
2023-05-08 00:20:17 -0300

    feat: adicionado remover e modal de visualização

--- src/app/estado/listar-estado/listar-estado/listar-estado.component.ts
+++ src/app/estado/listar-estado/listar-estado/listar-estado.component.ts
@@ -1,6 +1,8 @@
 import { ServiceService } from './../../services/service.service';
 import { Estado } from 'src/app/shared';
 import { Component, OnInit } from '@angular/core';
+import { ModalEstadoComponent } from '../../modal-estado/modal-estado/modal-estado.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-listar-estado',
@@ -10,7 +12,10 @@ import { Component, OnInit } from '@angular/core';
 export class ListarEstadoComponent implements OnInit {
   estados: Estado[] = [];
 
-  constructor(private estadoService: ServiceService) {}
+  constructor(
+    private estadoService: ServiceService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.estados = this.listarTodos();
@@ -18,5 +23,17 @@ export class ListarEstadoComponent implements OnInit {
 
   listarTodos(): Estado[] {
     return this.estadoService.listarTodos();
+  }
+
+  remover($event: any, estado: Estado): void {
+    $event.preventDefault();
+    if (confirm(`Deseja realmente remover a pessoa ${estado.nome}?`)) {
+      this.estadoService.remover(estado.id!);
+      this.estados = this.listarTodos();
+    }
+  }
+  abriModal(estado: Estado) {
+    const modalRef = this.modalService.open(ModalEstadoComponent);
+    modalRef.componentInstance.estado = estado;
   }
 }
--- src/app/estado/modal-estado/modal-estado/modal-estado.component.ts
+++ src/app/estado/modal-estado/modal-estado/modal-estado.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Estado } from 'src/app/shared';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-estado',
   templateUrl: './modal-estado.component.html',
-  styleUrls: ['./modal-estado.component.css']
+  styleUrls: ['./modal-estado.component.css'],
 })
 export class ModalEstadoComponent implements OnInit {
+  @Input() estado!: Estado;
+  constructor(public activeModal: NgbActiveModal) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
